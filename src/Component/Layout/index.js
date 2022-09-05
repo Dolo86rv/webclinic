f828c596f7ea0fe16a560135d0573e9269358b49
Dolo86rv <dolorv86@gmail.com>
2022-09-05 16:18:39 +0200

    se agrega Contact

--- src/Component/Layout/index.js
+++ src/Component/Layout/index.js
@@ -4,21 +4,21 @@ import { Home } from '../Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Servicio } from '../Servicios'
 import { Footer } from '../Footer'
+import { Contact } from '../Contact'
 
 export const Layout = () => {
   return (
-    <>
-    <BrowserRouter>
-      <Header />
-      <main className="flex-1 flex-grow">
-      <Routes>
-          <Route exact path='/' element={<Home />}/>
-          <Route exact path='/servicio' element={<Servicio />}/>
-        </Routes>
-      </main>
-      <Footer />
-    </BrowserRouter>
-    </>
+    <div className="flex flex-col h-screen w-full">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route exact path='/' element={<Home />}/>
+            <Route exact path='/servicio' element={<Servicio />}/>
+            <Route exact path='/contact' element={<Contact />}/>
+          </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
